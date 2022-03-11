610e3dbda46be0f96859fe96495d944ec4eb017e
Sena-GA <sena1995girma@gmail.com>
2022-03-11 20:13:08 +0000

    Prints the lowercase alphabet in reverse

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints the lowercase alphabet in reverse,
- * 
+ * main - prints the lowercase alphabet in reverse
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
