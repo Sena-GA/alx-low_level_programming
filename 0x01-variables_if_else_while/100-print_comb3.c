96d54c9ae827081611c3fa2c652a366dd7e62fbb
Sena-GA <sena1995girma@gmail.com>
2022-03-11 22:45:10 +0000

    Prints all possible different combinations of two digits, separated by ,

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 00 to 99
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != 57 || j != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
